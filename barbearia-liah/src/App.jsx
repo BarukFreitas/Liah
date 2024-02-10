a0fdbf06d89dd209fa3ca5cb5664c21deeec12c9
Baruk Freitas <baruk.freitas@gmail.com>
2024-02-10 00:02:50 -0300

    App criado

--- barbearia-liah/src/App.jsx
+++ barbearia-liah/src/App.jsx
@@ -1,14 +1,49 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div>olá react</div>
-  )
+    <>
+    <header className="menu">
+      <img src="imagens/logo.png" className="logo" alt="Logo" />
+      <a href="#">
+        <div className="menu-burguer">
+          <div className="barra"></div>
+          <div className="barra"></div>
+          <div className="barra"></div>
+        </div>
+      </a>
+    </header>
+    <div className="search-bar" aria-placeholder="Buscar Serviço">
+      <img src="imagens/icone-lupa.svg" className="lupa" alt="Ícone de Lupa" />
+      <p className="busca">Buscar Serviço...</p>
+    </div>
+    <div className="banner">
+      <p>Banner</p>
+    </div>
+    <span className="texto-1">
+      <p>Qual serviço gostaria de agendar?</p>
+    </span>
+    <div className="container">
+      <div className="card-1">
+        <img src="imagens/icone-cabelo.svg" alt="Ícone Cabelo" />
+        <p className="texto-container">Cabelo</p>
+      </div>
+      <div className="card-2">
+        <img src="imagens/icone-barba.svg" alt="Ícone Barba" />
+        <p className="texto-container">Barba</p>
+      </div>
+      <div className="card-3">
+        <img src="imagens/icone-cabelo-e-barba.svg" alt="Ícone Cabelo e Barba" />
+        <p className="texto-container">Cabelo e barba</p>
+      </div>
+      <div className="card-4">
+        <img src="imagens/icone-outros.svg" alt="Ícone Outros" />
+        <p className="texto-container">Outros</p>
+      </div>
+    </div>
+  </>
+);
 }
 
 export default App
